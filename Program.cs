d3a743d462c63a8eddb2438c5189df642470e416
Jere Käpyaho <jere@coniferproductions.com>
2021-07-18 06:31:18 +0300

    Rudimentary single patch dump in the K4 manual blank chart format

--- Program.cs
+++ Program.cs
@@ -300,6 +300,306 @@ namespace K4Tool
             return sb.ToString();
         }
 
+        private static string MakeSinglePatchText(SinglePatch singlePatch)
+        {
+            string MakeSingleColumnRow(string label, string value)
+            {
+                return String.Format($"{label,-10}{value}");
+            }
+
+            string MakeTwoColumnRow(string category, string label, string value, bool isFirst = false)
+            {
+                StringBuilder sb = new StringBuilder();
+
+                string space = " ";
+                if (isFirst)
+                {
+                    sb.Append(String.Format($"{category,-10}"));
+                }
+                else
+                {
+                    sb.Append(String.Format($"{space,-10}"));
+                }
+
+                sb.Append(String.Format($"{label,-20} {value}"));
+
+                return sb.ToString();
+            }
+
+            string CenteredString(string s, int desiredLength)
+            {
+                if (s.Length >= desiredLength)
+                {
+                    return s;
+                }
+                int firstPad = (s.Length + desiredLength) / 2;
+                return s.PadLeft(firstPad).PadRight(desiredLength);
+            }
+
+            List<string> lines = new List<string>();
+
+            lines.Add(MakeSingleColumnRow("Volume", singlePatch.Volume.ToString()));
+            lines.Add(MakeSingleColumnRow("Effect", singlePatch.Effect.ToString()));
+            lines.Add(MakeSingleColumnRow("Submix ch", singlePatch.Submix.ToString()));
+            lines.Add(MakeSingleColumnRow("Name", singlePatch.Name));
+            lines.Add(MakeTwoColumnRow("Common", "Source Mode", singlePatch.SourceMode.ToString(), true));
+
+            StringBuilder amValue = new StringBuilder();
+            if (singlePatch.AM12)
+            {
+                amValue.Append("1>2");
+            }
+            if (singlePatch.AM34)
+            {
+                amValue.Append(" 3>4");
+            }
+            lines.Add(MakeTwoColumnRow("Common", "AM", amValue.ToString()));
+
+            lines.Add(MakeTwoColumnRow("Common", "Poly Mode", singlePatch.PolyphonyMode.ToString()));
+            lines.Add(MakeTwoColumnRow("Common", "Bender Range", singlePatch.PitchBendRange.ToString()));
+            lines.Add(MakeTwoColumnRow("Common", "Press Freq", singlePatch.PressureFreq.ToString()));
+            lines.Add(MakeTwoColumnRow("Common", "Wheel Assign", singlePatch.WheelAssign.ToString()));
+            lines.Add(MakeTwoColumnRow("Common", "      Depth", singlePatch.WheelDepth.ToString()));
+            lines.Add(MakeTwoColumnRow("Common", "Auto Bend Time", singlePatch.AutoBend.Time.ToString()));
+            lines.Add(MakeTwoColumnRow("Common", "          Depth", singlePatch.AutoBend.Depth.ToString()));
+            lines.Add(MakeTwoColumnRow("Common", "          KS Time", singlePatch.AutoBend.KeyScalingTime.ToString()));
+            lines.Add(MakeTwoColumnRow("Common", "          Vel Depth", singlePatch.AutoBend.VelocityDepth.ToString()));
+
+            lines.Add(MakeTwoColumnRow("LFO", "Vibrato Shape", singlePatch.Vibrato.Shape.ToString(), true));
+            lines.Add(MakeTwoColumnRow("LFO", "        Speed", singlePatch.Vibrato.Speed.ToString()));
+            lines.Add(MakeTwoColumnRow("LFO", "        Depth", singlePatch.Vibrato.Depth.ToString()));
+            lines.Add(MakeTwoColumnRow("LFO", "        Press Depth", singlePatch.Vibrato.Pressure.ToString()));
+
+            lines.Add(MakeTwoColumnRow("LFO", "DCF-LFO Shape", singlePatch.LFO.Shape.ToString()));
+            lines.Add(MakeTwoColumnRow("LFO", "        Speed", singlePatch.LFO.Speed.ToString()));
+            lines.Add(MakeTwoColumnRow("LFO", "        Delay", singlePatch.LFO.Delay.ToString()));
+            lines.Add(MakeTwoColumnRow("LFO", "        Depth", singlePatch.LFO.Depth.ToString()));
+            lines.Add(MakeTwoColumnRow("LFO", "        Press Depth", singlePatch.LFO.PressureDepth.ToString()));
+
+            string space = " ";
+            lines.Add(String.Format("{0,-30}{1}{2}{3}{4}", space, CenteredString("S1", 10), CenteredString("S2", 10), CenteredString("S3", 10), CenteredString("S4", 10)));
+
+            StringBuilder sourceValues = new StringBuilder();
+            foreach (Source source in singlePatch.Sources)
+            {
+                sourceValues.Append(CenteredString(source.Delay.ToString(), 10));
+            }
+            lines.Add(MakeTwoColumnRow("S-Common", "Delay", sourceValues.ToString(), true));
+
+            sourceValues.Clear();
+            foreach (Source source in singlePatch.Sources)
+            {
+                sourceValues.Append(CenteredString(source.VelocityCurve.ToString(), 10));
+            }
+            lines.Add(MakeTwoColumnRow("S-Common", "Vel curve", sourceValues.ToString(), false));
+
+            sourceValues.Clear();
+            foreach (Source source in singlePatch.Sources)
+            {
+                sourceValues.Append(CenteredString(source.KeyScalingCurve.ToString(), 10));
+            }
+            lines.Add(MakeTwoColumnRow("S-Common", "KS curve", sourceValues.ToString(), false));
+
+            sourceValues.Clear();
+            foreach (Source source in singlePatch.Sources)
+            {
+                sourceValues.Append(CenteredString(source.WaveNumber.ToString(), 10));
+            }
+            lines.Add(MakeTwoColumnRow("DCO", "Wave", sourceValues.ToString(), true));
+
+            sourceValues.Clear();
+            foreach (Source source in singlePatch.Sources)
+            {
+                sourceValues.Append(CenteredString(source.KeyTrack.ToString(), 10));
+            }
+            lines.Add(MakeTwoColumnRow("DCO", "Key Track", sourceValues.ToString(), false));
+
+            sourceValues.Clear();
+            foreach (Source source in singlePatch.Sources)
+            {
+                sourceValues.Append(CenteredString(source.Coarse.ToString(), 10));
+            }
+            lines.Add(MakeTwoColumnRow("DCO", "Coarse", sourceValues.ToString(), false));
+
+            sourceValues.Clear();
+            foreach (Source source in singlePatch.Sources)
+            {
+                sourceValues.Append(CenteredString(source.Fine.ToString(), 10));
+            }
+            lines.Add(MakeTwoColumnRow("DCO", "Fine", sourceValues.ToString(), false));
+
+            sourceValues.Clear();
+            foreach (Source source in singlePatch.Sources)
+            {
+                sourceValues.Append(CenteredString(source.PressureFrequency.ToString(), 10));
+            }
+            lines.Add(MakeTwoColumnRow("DCO", "Press freq", sourceValues.ToString(), false));
+
+            sourceValues.Clear();
+            foreach (Source source in singlePatch.Sources)
+            {
+                sourceValues.Append(CenteredString(source.Vibrato.ToString(), 10));
+            }
+            lines.Add(MakeTwoColumnRow("DCO", "Vib/A.bend", sourceValues.ToString(), false));
+
+            sourceValues.Clear();
+            foreach (Amplifier amplifier in singlePatch.Amplifiers)
+            {
+                sourceValues.Append(CenteredString(amplifier.EnvelopeLevel.ToString(), 10));
+            }
+            lines.Add(MakeTwoColumnRow("DCA", "Level", sourceValues.ToString(), true));
+
+            sourceValues.Clear();
+            foreach (Amplifier amplifier in singlePatch.Amplifiers)
+            {
+                sourceValues.Append(CenteredString(amplifier.Env.Attack.ToString(), 10));
+            }
+            lines.Add(MakeTwoColumnRow("DCA", "Attack", sourceValues.ToString(), false));
+
+            sourceValues.Clear();
+            foreach (Amplifier amplifier in singlePatch.Amplifiers)
+            {
+                sourceValues.Append(CenteredString(amplifier.Env.Decay.ToString(), 10));
+            }
+            lines.Add(MakeTwoColumnRow("DCA", "Decay", sourceValues.ToString(), false));
+
+            sourceValues.Clear();
+            foreach (Amplifier amplifier in singlePatch.Amplifiers)
+            {
+                sourceValues.Append(CenteredString(amplifier.Env.Sustain.ToString(), 10));
+            }
+            lines.Add(MakeTwoColumnRow("DCA", "Sustain", sourceValues.ToString(), false));
+
+            sourceValues.Clear();
+            foreach (Amplifier amplifier in singlePatch.Amplifiers)
+            {
+                sourceValues.Append(CenteredString(amplifier.Env.Release.ToString(), 10));
+            }
+            lines.Add(MakeTwoColumnRow("DCA", "Release", sourceValues.ToString(), false));
+
+            sourceValues.Clear();
+            foreach (Amplifier amplifier in singlePatch.Amplifiers)
+            {
+                sourceValues.Append(CenteredString(amplifier.LevelMod.VelocityDepth.ToString(), 10));
+            }
+            lines.Add(MakeTwoColumnRow("DCA Mod", "Vel Depth", sourceValues.ToString(), true));
+
+            sourceValues.Clear();
+            foreach (Amplifier amplifier in singlePatch.Amplifiers)
+            {
+                sourceValues.Append(CenteredString(amplifier.LevelMod.PressureDepth.ToString(), 10));
+            }
+            lines.Add(MakeTwoColumnRow("DCA Mod", "Press Depth", sourceValues.ToString(), false));
+
+            sourceValues.Clear();
+            foreach (Amplifier amplifier in singlePatch.Amplifiers)
+            {
+                sourceValues.Append(CenteredString(amplifier.LevelMod.KeyScalingDepth.ToString(), 10));
+            }
+            lines.Add(MakeTwoColumnRow("DCA Mod", "KS Depth", sourceValues.ToString(), false));
+
+            sourceValues.Clear();
+            foreach (Amplifier amplifier in singlePatch.Amplifiers)
+            {
+                sourceValues.Append(CenteredString(amplifier.TimeMod.AttackVelocity.ToString(), 10));
+            }
+            lines.Add(MakeTwoColumnRow("DCA Mod", "Time Mod Attack", sourceValues.ToString(), false));
+
+            sourceValues.Clear();
+            foreach (Amplifier amplifier in singlePatch.Amplifiers)
+            {
+                sourceValues.Append(CenteredString(amplifier.TimeMod.ReleaseVelocity.ToString(), 10));
+            }
+            lines.Add(MakeTwoColumnRow("DCA Mod", "         Release", sourceValues.ToString(), false));
+
+            sourceValues.Clear();
+            foreach (Amplifier amplifier in singlePatch.Amplifiers)
+            {
+                sourceValues.Append(CenteredString(amplifier.TimeMod.KeyScaling.ToString(), 10));
+            }
+            lines.Add(MakeTwoColumnRow("DCA Mod", "         KS", sourceValues.ToString(), false));
+
+            lines.Add(String.Format("{0,-30}{1}{2}", space, CenteredString("F1", 20), CenteredString("F2", 20)));
+
+            lines.Add(MakeTwoColumnRow("DCF", "Cutoff",
+                String.Format("{0}{1}",
+                    CenteredString(singlePatch.Filter1.Cutoff.ToString(), 20),
+                    CenteredString(singlePatch.Filter2.Cutoff.ToString(), 20)), true));
+
+            lines.Add(MakeTwoColumnRow("DCF", "Resonance",
+                String.Format("{0}{1}",
+                    CenteredString(singlePatch.Filter1.Resonance.ToString(), 20),
+                    CenteredString(singlePatch.Filter2.Resonance.ToString(), 20))));
+
+            lines.Add(MakeTwoColumnRow("DCF", "Vel Depth",
+                String.Format("{0}{1}",
+                    CenteredString(singlePatch.Filter1.CutoffMod.VelocityDepth.ToString(), 20),
+                    CenteredString(singlePatch.Filter2.CutoffMod.VelocityDepth.ToString(), 20))));
+
+            lines.Add(MakeTwoColumnRow("DCF", "KS Depth",
+                String.Format("{0}{1}",
+                    CenteredString(singlePatch.Filter1.CutoffMod.KeyScalingDepth.ToString(), 20),
+                    CenteredString(singlePatch.Filter2.CutoffMod.KeyScalingDepth.ToString(), 20))));
+
+            lines.Add(MakeTwoColumnRow("DCF", "LFO",
+                String.Format("{0}{1}",
+                    CenteredString(singlePatch.Filter1.IsLFO.ToString(), 20),
+                    CenteredString(singlePatch.Filter2.IsLFO.ToString(), 20))));
+
+            lines.Add(MakeTwoColumnRow("DCF Mod", "Env Depth",
+                String.Format("{0}{1}",
+                    CenteredString(singlePatch.Filter1.EnvelopeDepth.ToString(), 20),
+                    CenteredString(singlePatch.Filter2.EnvelopeDepth.ToString(), 20)), true));
+
+            lines.Add(MakeTwoColumnRow("DCF Mod", "Vel Depth",
+                String.Format("{0}{1}",
+                    CenteredString(singlePatch.Filter1.EnvelopeVelocityDepth.ToString(), 20),
+                    CenteredString(singlePatch.Filter2.EnvelopeVelocityDepth.ToString(), 20))));
+
+            lines.Add(MakeTwoColumnRow("DCF Mod", "Attack",
+                String.Format("{0}{1}",
+                    CenteredString(singlePatch.Filter1.Env.Attack.ToString(), 20),
+                    CenteredString(singlePatch.Filter2.Env.Attack.ToString(), 20))));
+
+            lines.Add(MakeTwoColumnRow("DCF Mod", "Decay",
+                String.Format("{0}{1}",
+                    CenteredString(singlePatch.Filter1.Env.Decay.ToString(), 20),
+                    CenteredString(singlePatch.Filter2.Env.Decay.ToString(), 20))));
+
+            lines.Add(MakeTwoColumnRow("DCF Mod", "Sustain",
+                String.Format("{0}{1}",
+                    CenteredString(singlePatch.Filter1.Env.Sustain.ToString(), 20),
+                    CenteredString(singlePatch.Filter2.Env.Sustain.ToString(), 20))));
+
+            lines.Add(MakeTwoColumnRow("DCF Mod", "Release",
+                String.Format("{0}{1}",
+                    CenteredString(singlePatch.Filter1.Env.Release.ToString(), 20),
+                    CenteredString(singlePatch.Filter2.Env.Release.ToString(), 20))));
+
+            lines.Add(MakeTwoColumnRow("DCF Mod", "Time Mod Attack",
+                String.Format("{0}{1}",
+                    CenteredString(singlePatch.Filter1.TimeMod.AttackVelocity.ToString(), 20),
+                    CenteredString(singlePatch.Filter2.TimeMod.AttackVelocity.ToString(), 20))));
+
+            lines.Add(MakeTwoColumnRow("DCF Mod", "         Release",
+                String.Format("{0}{1}",
+                    CenteredString(singlePatch.Filter1.TimeMod.ReleaseVelocity.ToString(), 20),
+                    CenteredString(singlePatch.Filter2.TimeMod.ReleaseVelocity.ToString(), 20))));
+
+            lines.Add(MakeTwoColumnRow("DCF Mod", "         KS",
+                String.Format("{0}{1}",
+                    CenteredString(singlePatch.Filter1.TimeMod.KeyScaling.ToString(), 20),
+                    CenteredString(singlePatch.Filter2.TimeMod.KeyScaling.ToString(), 20))));
+
+            StringBuilder sb = new StringBuilder();
+            foreach (string line in lines)
+            {
+                sb.Append(line);
+                sb.Append("\n");
+            }
+            return sb.ToString();
+        }
+
         public static int RunDumpAndReturnExitCode(DumpOptions opts)
         {
             string fileName = opts.FileName;
@@ -318,10 +618,11 @@ namespace K4Tool
                 {
                     string patchId = PatchUtil.GetPatchName(patchNumber);
                     Console.WriteLine(patchId);
-                    Console.WriteLine(sp.ToString());
+                    Console.WriteLine(MakeSinglePatchText(sp));
                     patchNumber++;
                 }
 
+/*
                 patchNumber = 0;
                 Console.WriteLine("Multi patches:");
                 foreach (MultiPatch mp in bank.Multis)
@@ -331,7 +632,7 @@ namespace K4Tool
                     Console.WriteLine(mp.ToString());
                     patchNumber++;
                 }
-
+*/
                 Console.WriteLine("Drum and effect patches: later");
 
                 return 0;
@@ -553,9 +854,9 @@ namespace K4Tool
 
         public static int RunWaveAndReturnExitCode(WaveOptions opts)
         {
-            for (int i = 1; i <= K4Tool.Wave.WaveCount; i++)
+            for (int i = 1; i <= Wave.WaveCount; i++)
             {
-                Console.WriteLine($"{i,3} {K4Tool.Wave.Names[i]}");
+                Console.WriteLine($"{i,3} {Wave.Names[i]}");
             }
 
             return 0;
